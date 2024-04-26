ad2ae9fbc482288321da1a3b0dbc8ac79ae0da88
Hamed-PC <hjafarzadeh511@gmail.com>
2024-04-26 09:49:53 +0100

    Adding test, adding more comments about the functions

--- main.dart
+++ main.dart
@@ -1,14 +1,20 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-class Messages {
+class Message {
+  int length;
   int seqNumber;
-  String Message;
-  Messages(this.seqNumber, this.Message);
+  String msg;
+  Message(this.length, this.seqNumber, this.msg);
 }
 
-Future<List<Messages>> processLogiFile(String filePath) async {
-  List<Messages> messages = [];
+/* @brief processLogiFile is a function that reads the logi.bin file and processes the content.
+ * It reads the file content and extracts the length, sequence number, and the message.
+ * @param filePath: A string that contains the path to the logi.bin file
+ * @return List<Message>: A list of Message objects that contains the length, sequence number, and the message
+ */
+Future<List<Message>> processLogiFile(String filePath) async {
+  List<Message> messages = [];
   // Make sure that logi.bin exists
   if (!File(filePath).existsSync()) {
     throw ('$filePath not found');
@@ -27,18 +33,52 @@ Future<List<Messages>> processLogiFile(String filePath) async {
     String message =
         String.fromCharCodes(logiFileContent.sublist(i + 8, i + 8 + length));
     // print('i: $i Length: $length SeqNumber: $seqNumber, Message: $message'); // For Debugging purpose
-    messages.add(Messages(seqNumber, message));
+    messages.add(Message(length, seqNumber, message));
     i += 8 + length;
   }
 
   return messages;
 }
 
+
+/* @brief test is a function that tests the output of the processLogiFile function.
+ * It checks if the output is as expected.
+ * @param results: A list of Message objects that contains the length, sequence number, and the message
+ * @return bool: A boolean value that indicates if the test passed or failed
+ */
+bool test(List<Message> results) {
+  if (results.length != 3) {
+    throw ("Test failed: Expected 3 messages, but got ${results.length}");
+  }
+  if (results[0].seqNumber != 0 ||
+      results[0].msg != "Hello, welcome to Logitech!") {
+    throw ("Test failed: Expected SeqNumber: 1, Message: Hello, welcome to Logitech!, but got SeqNumber: ${results[0].seqNumber}, Message: ${results[0].msg}");
+  }
+  if (results[1].seqNumber != 1 || results[1].msg.length != results[1].length) {
+    throw ("Test failed: Expected SeqNumber: 2, ${results[1].seqNumber}, and Message length: ${results[1].length}, but got SeqNumber: ${results[1].seqNumber}, Message length: ${results[1].length}");
+  }
+  if (results[2].seqNumber != 2 ||
+      results[2].msg != "This is the final message, Goodbye") {
+    throw ("Test failed: Expected SeqNumber: 2, Message: This is the final message, Goodbye, but got SeqNumber: ${results[2].seqNumber}, Message: ${results[2].msg}");
+  }
+
+  return true;
+}
+
+
 Future main() async {
-  var results = await processLogiFile('logi.bin');
-  for (var result in results) {
-    print("=====================================");
-    print('SeqNumber: ${result.seqNumber}, Message: ${result.Message}');
+  try {
+    var results = await processLogiFile('logi.bin');
+    for (var result in results) {
+      print("=====================================");
+      print('SeqNumber: ${result.seqNumber}, Message: ${result.msg}');
+    }
+    print("\r\n \r\n===================TEST==================");
+    if (test(results)) {
+      print("Test Passed.");
+    }
+  } catch (e) {
+    print(e);
   }
 
   return 0;
